1d2f5365c792ac72f08fa9de9c25a041ce45c112
Damon Finch <ronaldpelham51@gmail.com>
2023-05-05 16:30:27 -0400

    mobile responsive for admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,13 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import Users from 'src/pages/admin/Users'
 import News from 'src/pages/admin/News'
 import Dashboard from 'src/pages/admin/Dashboard'
-import AdminSide from 'src/components/admin/layouts/AdminSide'
+import Header from 'src/components/admin/layouts/Header'
 
 const AdminLayout = () => {
   return (
     <div style={{ display: 'flex' }}>
-      <AdminSide />
-      <div style={{ padding: '0px 50px', width: '100%', display: 'flex', alignItems: 'center', backgroundColor: '#eee' }}>
+      <Header />
+      <div style={{ padding: '150px 50px', width: '100%', display: 'flex', alignItems: 'center', backgroundColor: '#eee' }}>
         <Routes>
           <Route path="/*" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
